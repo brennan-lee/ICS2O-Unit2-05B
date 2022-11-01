b0c44b0bc6c7c573ab70f29c2e9b811f133c81ec
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-01 14:55:09 -0400

    names

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Brennan Lee All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Brennan Lee
+// Created on: Sep 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,14 +10,26 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit2-05B", {
+    scope: "/ICS2O-Unit2-05B/",
   })
 }
 
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+
+function calculate() {
+  const hours = parseInt(
+    document.getElementById("number-of-hours-worked").value
+  )
+  const rate = parseInt(document.getElementById("Hourly-rate").value)
+
+  const take = hours * rate * 0.18
+  const wage = hours * rate * (1.0 - 0.18)
+
+  document.getElementById("take").innerHTML =
+    "The governemnt will take: " + take.toFixed(2) + "$"
+  document.getElementById("wage").innerHTML =
+    "your pay will be: " + wage.toFixed(2) + "$"
 }
