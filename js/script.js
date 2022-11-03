56ce28eb51f1e6c8887051eacec3f634114ccb4e
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-03 11:56:53 -0400

    comment

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Brennan Lee All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Brennan Lee
+// Created on: oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
