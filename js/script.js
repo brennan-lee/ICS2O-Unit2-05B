2972892a1761247a010de6e3a0ab2942d12b20db
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-03 11:50:01 -0400

    2-05 put in for tax

--- js/script.js
+++ js/script.js
@@ -15,9 +15,17 @@ if (navigator.serviceWorker) {
   })
 }
 
-/**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function calculate() {
+  const hours = parseInt(
+    document.getElementById("number-of-hours-worked").value
+  )
+  const rate = parseInt(document.getElementById("Hourly-rate").value)
+
+  const take = hours * rate * 0.18
+  const wage = hours * rate * (1.0 - 0.18)
+
+  document.getElementById("take").innerHTML =
+    "The governemnt will take: " + take.toFixed(2) + "$"
+  document.getElementById("wage").innerHTML =
+    "your pay will be: " + wage.toFixed(2) + "$"
 }
